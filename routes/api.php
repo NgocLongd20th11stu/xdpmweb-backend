18e871d3ffd4352ac896fefc430717c18b87dbd3
Ngoc Long <dangngoclong28012002@gmail.com>
2026-03-13 13:41:24 +0700

    fix users api

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,13 @@ Route::post('/admin/login', [AuthController::class,'authenticate']);
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+
+Route::get('/users', function () {
+    return User::all();
+    });
+
+
+
 Route::group(['middleware' => 'auth:sanctum'], function(){
     // Route::get('categories',[CategoryController::class,'index']);
     // Route::get('categories/{id}',[CategoryController::class,'show']);
@@ -33,7 +40,6 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::resource('products', ProductController::class);
     Route::post('temp-images', [TempImageController::class,'store']);
 
-    Route::get('/users', function () {
-    return User::all();
-});
+    
+    
 });
